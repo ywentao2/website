1c57eb6cee8a4e6bb1f56f1509b0e3bb8277b412
Austin Yang <70672670+ywentao2@users.noreply.github.com>
2025-01-13 11:47:05 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <body className="bg-slate-500">
+        <div className="container mx-auto p-4">
+          <h1 className="text-4xl font-bold text-white">Hello, Tailwind CSS!</h1>
+          <p className="text-lg text-white">This is a simple Tailwind CSS example.</p>
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default App
